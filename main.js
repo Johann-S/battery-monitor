19f1707bc5096281e4021675f065dff4c33fe32c
Johann-S <johann.servoire@gmail.com>
2019-07-10 15:27:11 +0200

    add more battery icons

--- main.js
+++ main.js
@@ -4,45 +4,44 @@ const { getBatteryInformation, getBatteryImage } = require('./src/back/battery-h
 
 let appTray
 app.on('ready', () => {
-  getBatteryInformation()
-    .then(batteryInformation => {
-      const iconName = getBatteryImage(batteryInformation.hasbattery, batteryInformation.percent)
-      const iconPath = path.join(`${__dirname}/battery-icon`, iconName)
-      const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        icon: iconPath,
-        webPreferences: {
-          nodeIntegration: true
-        }
-      })
-
-      const contextMenu = Menu.buildFromTemplate([{
-          label: 'Show',
-          click() {
-            win.show()
-          }
-        },
-        {
-          label: 'Quit',
-          click() {
-            app.quit()
-          }
-        }
-      ])
-
-      appTray = new Tray(iconPath)
-      appTray.setContextMenu(contextMenu)
-
-      ipcMain.on('get-battery-info', event => {
-        getBatteryInformation()
-          .then(batteryInformation => event.reply('battery-info', batteryInformation))
-      })
-
-      win.on('minimize', event => {
-        event.preventDefault()
-        win.hide()
-      })
-      win.loadFile('index.html')
+  getBatteryInformation().then(({ hasbattery, percent }) => {
+    const iconName = getBatteryImage(hasbattery, percent)
+    const iconPath = path.join(`${__dirname}/battery-icon`, iconName)
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      icon: iconPath,
+      webPreferences: {
+        nodeIntegration: true
+      }
     })
+
+    const contextMenu = Menu.buildFromTemplate([{
+        label: 'Show',
+        click() {
+          win.show()
+        }
+      },
+      {
+        label: 'Quit',
+        click() {
+          app.quit()
+        }
+      }
+    ])
+
+    appTray = new Tray(iconPath)
+    appTray.setContextMenu(contextMenu)
+
+    ipcMain.on('get-battery-info', event => {
+      getBatteryInformation()
+        .then(batteryInformation => event.reply('battery-info', batteryInformation))
+    })
+
+    win.on('minimize', event => {
+      event.preventDefault()
+      win.hide()
+    })
+    win.loadFile('index.html')
+  })
 })
