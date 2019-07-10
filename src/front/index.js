5f9dfd37403f5af0b10dae5633d159cfc510bac0
Johann-S <johann.servoire@gmail.com>
2019-07-10 14:03:34 +0200

    disable auto refresh and detect no battery

--- src/front/index.js
+++ src/front/index.js
@@ -6,21 +6,59 @@ const refreshInterval = 4000
 
 class App extends Component {
   componentDidMount() {
-    setInterval(() => ipcRenderer.send('get-battery-info'), refreshInterval)
-    ipcRenderer.on('battery-info', (event, batteryInfo) => this.setState({ batteryInfo: batteryInfo }))
+    this.chkAutoRefreshEl = document.getElementById('chkAutoRefresh')
+    this.chkAutoRefreshEl.checked = true
+    this.setState({ refreshAuto: true, batteryInfo: undefined })
+    this.initAutoRefresh()
+
+    ipcRenderer.on('battery-info', (event, batteryInfo) => this.setState({ batteryInfo }))
     ipcRenderer.send('get-battery-info')
   }
 
+  initAutoRefresh() {
+    this.intervalRefresh = setInterval(() => ipcRenderer.send('get-battery-info'), refreshInterval)
+  }
+
   renderBatteryInfo() {
+    const { batteryInfo } = this.state
+
+    if (!batteryInfo.hasbattery) {
+      if (this.state.refreshAuto) {
+        this.changeRefreshAuto()
+      }
+
+      return hx`
+      <div class="alert alert-danger text-center mt-2" role="alert">
+        You don't have a battery
+      </div>
+      `
+    }
+
     return hx`
       <p>${this.state.batteryInfo.percent}</p>
     `
   }
 
+  changeRefreshAuto() {
+    this.setState({ refreshAuto: !this.state.refreshAuto })
+
+    if (!this.state.refreshAuto) {
+      clearInterval(this.intervalRefresh)
+    } else {
+      this.initAutoRefresh()
+    }
+
+    this.chkAutoRefreshEl.checked = this.state.refreshAuto
+  }
+
   render({}, { batteryInfo }) {
     return hx`
     <div class="container">
-      <h1>Monitoring your batterie</h1>
+      <h1 class="text-center">Monitoring your batterie</h1>
+      <div class="custom-control custom-switch d-inline-block" onClick=${() => this.changeRefreshAuto()}>
+        <input id="chkAutoRefresh" type="checkbox" class="custom-control-input">
+        <label class="custom-control-label align-middle" for="customSwitch1">Refresh auto</label>
+      </div>
       ${!batteryInfo ? '' : this.renderBatteryInfo()}
     </div>
     `
