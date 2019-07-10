2d2d7fcb5314a51f2bbf75e9028e274e9c6529a9
Johann-S <johann.servoire@gmail.com>
2019-07-10 16:32:20 +0200

    display all batterie percents

--- src/back/battery-helper.js
+++ src/back/battery-helper.js
@@ -1,5 +1,15 @@
 const si = require('systeminformation')
 const batteryStates = {
+  alert: 'alert.ico',
+  '10': '10.ico',
+  '20': '20.ico',
+  '30': '30.ico',
+  '40': '40.ico',
+  '50': '50.ico',
+  '60': '60.ico',
+  '70': '70.ico',
+  '80': '80.ico',
+  '90': '90.ico',
   full: 'full.ico',
   unknown: 'unknown.ico'
 }
@@ -16,6 +26,46 @@ const getBatteryInformation = () => new Promise(resolve => {
 const getBatteryImage = (hasBattery, percent) => {
   if (!hasBattery) {
     return batteryStates.unknown
+  }
+
+  if (percent < 10) {
+    return batteryStates.alert
+  }
+
+  if (percent < 20) {
+    return batteryStates['10']
+  }
+
+  if (percent < 30) {
+    return batteryStates['20']
+  }
+
+  if (percent < 40) {
+    return batteryStates['30']
+  }
+
+  if (percent < 50) {
+    return batteryStates['40']
+  }
+
+  if (percent < 60) {
+    return batteryStates['50']
+  }
+
+  if (percent < 70) {
+    return batteryStates['60']
+  }
+
+  if (percent < 80) {
+    return batteryStates['70']
+  }
+
+  if (percent < 90) {
+    return batteryStates['80']
+  }
+
+  if (percent < 100) {
+    return batteryStates['90']
   }
 
   return batteryStates.full
