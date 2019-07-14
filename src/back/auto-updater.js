7b5e8021ed3de2e85b34e4d4a391eefcdf1691db
Johann-S <johann.servoire@gmail.com>
2019-07-14 22:13:40 +0200

    improve updater

--- src/back/auto-updater.js
+++ src/back/auto-updater.js
@@ -2,15 +2,14 @@ const { app } = require('electron')
 const { autoUpdater } = require('electron-updater')
 const log = require('electron-log')
 
-log.transports.file.level = 'info'
-autoUpdater.logger = log
-
 const checkUpdates = () => {
   if (!app.isPackaged) {
     return
   }
 
-  autoUpdater.checkForUpdates()
+  log.transports.file.level = 'info'
+  autoUpdater.logger = log
+  autoUpdater.checkForUpdatesAndNotify()
 }
 
 module.exports = checkUpdates
